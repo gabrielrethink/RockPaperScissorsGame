9a7e5f4243687bf28f02931f35dea09d72df3cf6
fernando-henrique2001 <fernando.henrique@rethink.dev>
2022-03-31 15:40:07 -0300

    first commit

--- app.js
+++ app.js
@@ -1,74 +1,88 @@
 let userScore = 0;
 let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
-const computerScore_spam = document.getElementById("computer-score");
-const scoreBoard_div = document.querySelector(".score-board");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".user-score");
 const result_p = document.querySelector(".result > p");
-const result_spam = document.querySelector(".result > spam");
-const rock_div = document.getElementById("Rock");
-const paper_div = document.getElementById("Paper");
-const scissor_div = document.getElementById("Scissor");
-
-const getComputerChoice = () => {
-  const choices = ["Rock", "Paper", "Scissor"];
-  return choices[Math.floor(Math.random() * choices.length)];
-};
-
-const animationGlow = (userChoice, className) => {
-  document.getElementById(userChoice).classList.add(className);
-  setTimeout(() => {
-    document.getElementById(userChoice).classList.remove(className);
-  }, 1000);
-};
-
-const gameResult = (gameResult, winner) => {
-  if (winner === "win") {
-    userScore++;
-    userScore_span.innerHTML = userScore;
-    result_p.innerHTML = `${gameResult.userChoice} beats ${gameResult.cumputerChoice}`;
-    result_spam.innerHTML = "User Wins This Round";
-    animationGlow(gameResult.userChoice, "green-glow");
-  }
-  if (winner === "lose") {
-    computerScore++;
-    computerScore_spam.innerHTML = computerScore;
-    result_p.innerHTML = `${gameResult.userChoice} loses ${gameResult.cumputerChoice}`;
-    result_spam.innerHTML = "User Loses This Round";
-    animationGlow(gameResult.userChoice, "red-glow");
-  }
-  if (winner === "draw") {
-    result_p.innerHTML = `${gameResult.userChoice} draw with ${gameResult.cumputerChoice}`;
-    result_spam.innerHTML = "It's a Draw!! Nobody wins!";
-  }
-};
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
 
 const game = (userChoice) => {
-  const cumputerChoice = getComputerChoice();
-  switch (userChoice + "/" + cumputerChoice) {
-    case "Rock/Rock":
-    case "Paper/Paper":
-    case "Scissor/Scissor":
-      gameResult({ userChoice, cumputerChoice }, "draw");
-      break;
-    case "Rock/Scissor":
-    case "Paper/Rock":
-    case "Scissor/Paper":
-      gameResult({ userChoice, cumputerChoice }, "win");
-      break;
-    case "Scissor/Rock":
-    case "Rock/Paper":
-    case "Paper/Scissor":
-      gameResult({ userChoice, cumputerChoice }, "lose");
-      break;
-  }
-};
+    getWinner(userChoice, getComputerChoice());
+}
+
+const getWinner = (userChoice, computerChoice) => {
+    const match = userChoice + computerChoice;
+    switch (match) {
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            console.log("USER: ", String(userChoice).toUpperCase(), "|| COMPUTER: ", String(computerChoice).toUpperCase());
+            console.log("DRAW");
+            gameResult('draw');
+            break;
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            console.log("USER: ", String(userChoice).toUpperCase(), "|| COMPUTER: ", String(computerChoice).toUpperCase());
+            console.log("User win!");
+            gameResult('winner');
+            break;
+        case 'sr':
+        case 'rp':
+        case 'ps':
+            console.log("USER: ", String(userChoice).toUpperCase(), "|| COMPUTER: ", String(computerChoice).toUpperCase());
+            console.log("Computer win!");
+            gameResult('loser');
+            break;
+    }
+}
+
+const gameResult = (result) => {
+    switch (result) {
+        case 'draw':
+            result_p.innerHTML = 'Empate!'
+            break;
+        case 'winner':
+            userScore++;
+            userScore_span.innerHTML = userScore;
+            result_p.innerHTML = 'O usuario venceu!';
+            userScore_span.classList.add('score-green');
+            break;
+        case 'loser':
+            computerScore++;
+            computerScore_span.innerHTML = computerScore;
+            result_p.innerHTML = 'O Computador venceu!';
+            computerScore_span.classList.add('score-green');
+            break;
+
+    }
+    setTimeout( function() {
+        rock_div.classList.remove('green')
+        paper_div.classList.remove('green');
+        scissor_div.classList.remove('green');
+        userScore_span.classList.remove('score-green');
+        computerScore_span.classList.remove('score-green');
+    }, 700)
+}
+
+const getComputerChoice = () => {
+    const choices = ["r", "p", "s"]
+    return choices[Math.floor(Math.random() * choices.length)];
+}
 
 rock_div.addEventListener("click", () => {
-  game("Rock");
+    rock_div.classList.add('green');
+    game("r");
 });
+
 paper_div.addEventListener("click", () => {
-  game("Paper");
+    paper_div.classList.add('green');
+    game("p");
 });
+
 scissor_div.addEventListener("click", () => {
-  game("Scissor");
+    scissor_div.classList.add('green');
+    game("s");
 });
