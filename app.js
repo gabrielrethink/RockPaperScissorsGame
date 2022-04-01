28247746b75c10bba11e84741ef9891d95ee091c
anaclara-rethink <102759771+anaclara-rethink@users.noreply.github.com>
2022-04-01 13:59:48 -0300

    first commit

--- app.js
+++ app.js
@@ -1,74 +1,83 @@
+// alert("Está funcionando")
 let userScore = 0;
 let computerScore = 0;
-const userScore_span = document.getElementById("user-score");
+const userScore_spam = document.getElementById("user-score");
 const computerScore_spam = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_p = document.querySelector(".result > p");
 const result_spam = document.querySelector(".result > spam");
-const rock_div = document.getElementById("Rock");
-const paper_div = document.getElementById("Paper");
-const scissor_div = document.getElementById("Scissor");
+const result_placar = document.querySelector(".result > placar");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
 
 const getComputerChoice = () => {
-  const choices = ["Rock", "Paper", "Scissor"];
-  return choices[Math.floor(Math.random() * choices.length)];
+    const choices = ["Rock", "Scissor", "Paper"];
+    return choices[Math.floor(Math.random() * choices.length)];
 };
 
-const animationGlow = (userChoice, className) => {
-  document.getElementById(userChoice).classList.add(className);
-  setTimeout(() => {
-    document.getElementById(userChoice).classList.remove(className);
-  }, 1000);
+const iconChoice = (choice) => {
+    if(choice === "Rock")
+        return '<img src="images/rock.png" alt="" />'
+    if(choice === "Paper")
+        return '<img src="images/paper.png" alt="" />'
+    if(choice === "Scissor")
+        return '<img src="images/scissors.png" alt="" />'
+}
+
+const gameResult= (gameResult, winner) => {
+    if(winner === "win"){
+        userScore++;
+        userScore_spam.innerHTML = userScore;
+        result_p.innerHTML = `${gameResult.userChoice} beats ${gameResult.computerChoice}!`;
+        result_spam.innerHTML = `User Wins This Round`;
+        result_placar.innerHTML = ` ${iconChoice(gameResult.userChoice) } X ${iconChoice(gameResult.computerChoice)}`
+        // document.getElementById(gameResult.userChoice).classList.add("gree-glow");
+    }
+    if(winner === "lose"){
+        computerScore++;
+        computerScore_spam.innerHTML = computerScore;
+        result_p.innerHTML = `${gameResult.userChoice} loses ${gameResult.computerChoice}!`;
+        result_spam.innerHTML = "User Lost This Round";
+        result_placar.innerHTML = ` ${iconChoice(gameResult.userChoice) } X ${iconChoice(gameResult.computerChoice)}`
+        // document.getElementById(gameResult.userChoice).classList.add("gree-glow");    
+    }
+    if(winner === "draw"){
+        result_p.innerHTML = `${gameResult.userChoice} draw with ${gameResult.computerChoice}!`
+        result_spam.innerHTML = "It's a Draw! Nobody wins!";
+        result_placar.innerHTML = ` ${iconChoice(gameResult.userChoice) } X ${iconChoice(gameResult.computerChoice)}`
+    }
+}
+
+const game = (userChoice) =>{
+    const computerChoice = getComputerChoice();
+    switch(userChoice + "/" + computerChoice){
+        case "Rock/Rock":
+        case "Paper/Paper":
+        case "Scissor/Scissor":
+            gameResult({userChoice, computerChoice}, "draw");
+            break;
+        case "Rock/Scissor":
+        case "Paper/Scissor":
+        case "Scissor/Paper":
+            gameResult({userChoice, computerChoice}, "win");
+            break;
+        case "Scissor/Rock":
+        case "Rock/Paper":
+        case "Paper/Rock":
+            gameResult({userChoice, computerChoice}, "lose");
+            break;
+    }
 };
 
-const gameResult = (gameResult, winner) => {
-  if (winner === "win") {
-    userScore++;
-    userScore_span.innerHTML = userScore;
-    result_p.innerHTML = `${gameResult.userChoice} beats ${gameResult.cumputerChoice}`;
-    result_spam.innerHTML = "User Wins This Round";
-    animationGlow(gameResult.userChoice, "green-glow");
-  }
-  if (winner === "lose") {
-    computerScore++;
-    computerScore_spam.innerHTML = computerScore;
-    result_p.innerHTML = `${gameResult.userChoice} loses ${gameResult.cumputerChoice}`;
-    result_spam.innerHTML = "User Loses This Round";
-    animationGlow(gameResult.userChoice, "red-glow");
-  }
-  if (winner === "draw") {
-    result_p.innerHTML = `${gameResult.userChoice} draw with ${gameResult.cumputerChoice}`;
-    result_spam.innerHTML = "It's a Draw!! Nobody wins!";
-  }
-};
-
-const game = (userChoice) => {
-  const cumputerChoice = getComputerChoice();
-  switch (userChoice + "/" + cumputerChoice) {
-    case "Rock/Rock":
-    case "Paper/Paper":
-    case "Scissor/Scissor":
-      gameResult({ userChoice, cumputerChoice }, "draw");
-      break;
-    case "Rock/Scissor":
-    case "Paper/Rock":
-    case "Scissor/Paper":
-      gameResult({ userChoice, cumputerChoice }, "win");
-      break;
-    case "Scissor/Rock":
-    case "Rock/Paper":
-    case "Paper/Scissor":
-      gameResult({ userChoice, cumputerChoice }, "lose");
-      break;
-  }
-};
-
-rock_div.addEventListener("click", () => {
-  game("Rock");
+rock_div.addEventListener("click", () =>{
+    game("Rock");
 });
-paper_div.addEventListener("click", () => {
-  game("Paper");
+
+paper_div.addEventListener("click", () =>{
+    game("Paper");
 });
-scissor_div.addEventListener("click", () => {
-  game("Scissor");
+
+scissor_div.addEventListener("click", () =>{
+    game("Scissor");
 });
